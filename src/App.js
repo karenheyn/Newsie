c5572179e7692dadb809edbf8c9af860a2b1ea88
karenheyn <karenheyn@gmail.com>
2020-01-04 19:34:55 -0500

    changed App to class component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className='App'>
-      <nav className='nav'>News </nav>
-      <div className='content-container'>newsynews</div>
-      <div className='content-container'>newsynews</div>
-      <div className='content-container'>newsynews</div>
-      <footer></footer>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className='App'>
+        <nav className='nav'>News </nav>
+        <div className='content-container'>newsynews</div>
+        <div className='content-container'>newsynews</div>
+        <div className='content-container'>newsynews</div>
+        <footer></footer>
+      </div>
+    );
+  }
 }
 
 export default App;
