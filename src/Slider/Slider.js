f0be3ed09185f8e185bfe37198b4b038253bc9a2
karenheyn <karenheyn@gmail.com>
2020-01-04 20:39:19 -0500

    started class component for slider

---
+++ src/Slider/Slider.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./Slider.css"
+
+class Slider extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Slider;
