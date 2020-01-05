388f1b4c58cfaeebf5c0d1f0be54d35ea88dbe20
karenheyn <karenheyn@gmail.com>
2020-01-04 20:49:21 -0500

    added comments, rendered Slider in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Slider from "./Slider/Slider";
 const key = "37c291dbdee64f5c9b9601e9033fca5f";
 const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${key}`;
 
@@ -22,12 +23,15 @@ class App extends React.Component {
     console.log(this.state);
     const isLoading = this.state.isLoading;
     if (isLoading) {
-      return <h1>Loading ...</h1>;
+      return <h1>Loading ...</h1>; //this is going to be a loading animation
     }
     return (
       <div className='App'>
         <nav className='nav'>News </nav>
-        <div className='content-container'>newsynews</div>
+        <div className='content-container'>
+          newsynews
+          <Slider />
+        </div>
         <div className='content-container'>newsynews</div>
         <div className='content-container'>newsynews</div>
         <footer></footer>
