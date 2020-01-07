79d1bed5381d61e4823211fdb4cb58c03be6976c
karenheyn <karenheyn@gmail.com>
2020-01-07 18:20:45 -0500

    working on third fetch

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import "./App.css";
 import Slider from "./Slider/Slider";
 const key = "37c291dbdee64f5c9b9601e9033fca5f";
-const urlOne = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${key}`;
+const urlOne = `https://newsapi.org/v2/top-headlines?country=us&pageSize=100&apiKey=${key}`;
 // const urlTwo = `https://newsapi.org/v2/sources?language=en&country=us&apiKey=${key}`;
 
-const urlTwo = `https://newsapi.org/v2/everything?q=technology&from=${getDateParam()}&sortBy=publishedAt&apiKey=${key}`;
+const urlTwo = `https://newsapi.org/v2/top-headlines?category=technology&pageSize=100&language=en&country=us&sortBy=publishedAt&apiKey=${key}`;
+const urlThree = `https://newsapi.org/v2/top-headlines?category=sports&pageSize=100&language=en&country=us&sortBy=publishedAt&apiKey=${key}`;
 
 function getDateParam() {
   const d = new Date();
@@ -19,18 +20,22 @@ class App extends React.Component {
     super(props);
     this.state = {
       data: [],
-      catData: [],
+      techData: [],
+      sportsData: [],
       isLoading: false
     };
   }
   componentDidMount() {
     this.setState({ isLoading: true });
-    Promise.all([fetch(urlOne), fetch(urlTwo)])
-      .then(([res1, res2]) => Promise.all([res1.json(), res2.json()]))
-      .then(([data1, data2]) =>
+    Promise.all([fetch(urlOne), fetch(urlTwo), fetch(urlThree)])
+      .then(([res1, res2, res3]) =>
+        Promise.all([res1.json(), res2.json(), res3.json])
+      )
+      .then(([data1, data2, data3]) =>
         this.setState({
           data: data1.articles,
-          catData: data2,
+          techData: data2.articles,
+          sportsData: data3.articles,
           isLoading: false
         })
       )
@@ -42,7 +47,13 @@ class App extends React.Component {
       ...article,
       index: index
     }));
+    const techArticlesWithIndex = this.state.techData.map((article, index) => ({
+      //adds key value of index to obj arr so it can be used for slider
+      ...article,
+      index: index
+    }));
     console.log(this.state.catData); //remove later
+    console.log(this.state.data);
     const isLoading = this.state.isLoading;
     if (isLoading) {
       return <h1>Loading ...</h1>; //this is going to be a loading animation
@@ -51,10 +62,12 @@ class App extends React.Component {
       <div className='App'>
         <nav className='nav'>News </nav>
         <div className='content-container'>
-          newsynews
           <Slider data={articlesWithIndex} />
         </div>
-        <div className='content-container'>newsynews</div>
+        <div className='content-container'>
+          newsynews <Slider data={techArticlesWithIndex} />
+        </div>
+
         <div className='content-container'>newsynews</div>
         <footer></footer>
       </div>
